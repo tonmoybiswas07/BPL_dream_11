ce2aab7a8414d7040ccb42fb61040d8c2a791e78
Tonmoy Biswas <tonmoybiswas13900@gmail.com>
2026-09-08 22:25:01 +0600

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import Navbar from "./Components/Navbar/Navbar";
+
 function App() {
   return <>
   
-  <h1 className="text-5xl font-bold text-green-600">bpl dream 11</h1>
-
-  <button className="btn btn-error">Error</button>
+  <Navbar></Navbar>
   </>;
 }
 
