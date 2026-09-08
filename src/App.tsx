b0f56cb9739af3ad881270f17719292027486c59
Tonmoy Biswas <tonmoybiswas13900@gmail.com>
2026-09-08 21:53:55 +0600

    bpl dream 11 project started

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return <>
+  
+  <h1 className="text-5xl font-bold text-green-600">bpl dream 11</h1>
+
+  <button className="btn btn-error">Error</button>
+  </>;
+}
+
+export default App;
